c7e83d0f23bacb5a67a2402f76047a0f30519d79
Daniel Lindau <d.lindau@gmail.com>
2021-01-26 13:59:08 +0100

    Move templates to jar

--- src/main/java/io/curity/identityserver/plugin/SendEmailAction/SendEmailActionAuthenticationAction.java
+++ src/main/java/io/curity/identityserver/plugin/SendEmailAction/SendEmailActionAuthenticationAction.java
@@ -103,7 +103,7 @@ public final class SendEmailActionAuthenticationAction implements Authentication
 
             try
             {
-                _emailSender.sendEmail(recipientMail, email, "sendemailaction/email/message");
+                _emailSender.sendEmail(recipientMail, email, "email/message");
             }
             catch (RuntimeException exception)
             {
